a3d5a19e803c5b0c7cd187cb2097c51e4f347d72
puneeth <puneeth50@gmail.com>
2018-08-09 15:11:57 +0100

    adding console.log and HTML file to give the input value and get number of combinations

--- server/stairs_with_array1_2.js
+++ server/stairs_with_array1_2.js
@@ -2,12 +2,18 @@ function generate_Possible_stairs_for_1_2_combination(stairCaseValue) {
 	var n = parseInt(stairCaseValue)
 	var n1,n2,n3;	
 				n1 = 1, n2 = 1, n3 = 1;
-				for(i = 2; i < n; i++) {
+				for(i = 2; i <= n; i++) {
 					n3 = n1 + n2;
 					n1 = n2;
 					n2 = n3;
 				}
 
+				if(n>=0){
+				console.log("Number of possible paths = " +n3);
 				return n3;
+				}
+				else{
+				console.log("please enter positive integter starting from zero");
+				return "please enter positive integer starting with zero";
 				}
 			}
